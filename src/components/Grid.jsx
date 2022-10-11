6c1931b1e4c1e0bfb46ef6d63b043a951864a2f8
thomas78125 <thomas78125@gmail.com>
2022-10-11 22:07:18 +0200

    grid css

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
+import '../assets/css/grid.scss'
 
 const Grid = () => {
     return (
         <div className='container'>
             <h1 className='title'> Grid</h1>
-            <div className='wrap'>
-                <div className='bg-rainbow'></div>
-                <div className='bg-rainbow'></div>
-                <div className='bg-rainbow'></div>
-                <div className='bg-rainbow'></div>
-                <div className='bg-rainbow'></div>
-                <div className='bg-rainbow'></div>
-                <div className='bg-rainbow'></div>
-                   
+            <div class="grid">
+                <div class="one">Un</div>
+                <div class="two">Deux</div>
+                <div class="three">Trois</div>
+                <div class="four">Quatre</div>
+                <div class="five">Cinq</div>
+                <div class="six">Six</div>
+                <div class="seven">Seven</div>
+                <div class="height">Height</div>
+                <div class="nine">Nine</div>
+                <div class="ten">Ten</div>
+                <div class="eleven">Eleven</div>
+                <div class="twelve">Twelve</div>
+                <div class="thirdteen">Thirdteen</div>
+                <div class="fourteen">Fourteen</div>
+                <div class="fiveteen">Fiveteen</div>
+                <div class="sixteen">Sixteen</div>
+                <div class="seventeen">Seventeen</div>
+                <div class="heighteen">Heighteen</div>
+                <div class="nineteen">Nineteen</div>
             </div>
         </div>
     );
--- src/components/Wrap.jsx
+++ src/components/Wrap.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../assets/css/wrap.scss'
 
 const Wrap = () => {
     return (
